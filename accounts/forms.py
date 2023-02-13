01b18b66ec4167e52cb3b8a251aa3628673a272e
divdef <divdefwork@gmail.com>
2023-02-13 12:44:37 +0200

    Part 13

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,43 +6,42 @@ from .models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=250, required=True)
-    password = forms.CharField(max_length=250, required=True, widget=forms.PasswordInput)
-    
+    password = forms.CharField(max_length=250, required=True,
+                               widget=forms.PasswordInput)
 
 
 class UserRegistrationForm(forms.ModelForm):
-    
     class Meta:
         model = User
-        fields = ("username", "email", "password", )
+        fields = ("username", "email", "password",)
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
         model = self.Meta.model
         user = model.objects.filter(username__iexact=username)
-        
+
         if user.exists():
-            raise forms.ValidationError("A user with that name already exists")
-        
+            raise forms.ValidationError("Користувач із таким іменем уже існує")
+
         return self.cleaned_data.get('username')
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
         model = self.Meta.model
         user = model.objects.filter(email__iexact=email)
-        
-        if user.exists():
-            raise forms.ValidationError("A user with that email already exists")
-        
-        return self.cleaned_data.get('email')
 
+        if user.exists():
+            raise forms.ValidationError(
+                "Користувач із такою електронною адресою вже існує")
+
+        return self.cleaned_data.get('email')
 
     def clean_password(self):
         password = self.cleaned_data.get('password')
         confim_password = self.data.get('confirm_password')
-        
+
         if password != confim_password:
-            raise forms.ValidationError("Passwords do not match")
+            raise forms.ValidationError("Паролі не збігаються")
 
         return self.cleaned_data.get('password')
 
@@ -51,29 +50,31 @@ class UserProfileUpdateForm(forms.ModelForm):
     def _init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-
     class Meta:
         model = User
-        fields = ("first_name", "last_name", "username", "email", )
+        fields = ("first_name", "last_name", "username", "email",)
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
         model = self.Meta.model
-        user = model.objects.filter(username__iexact=username).exclude(pk=self.instance.pk)
-        
+        user = model.objects.filter(username__iexact=username).exclude(
+            pk=self.instance.pk)
+
         if user.exists():
-            raise forms.ValidationError("A user with that name already exists")
-        
+            raise forms.ValidationError("Користувач із таким іменем уже існує")
+
         return self.cleaned_data.get('username')
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
         model = self.Meta.model
-        user = model.objects.filter(email__iexact=email).exclude(pk=self.instance.pk)
-        
+        user = model.objects.filter(email__iexact=email).exclude(
+            pk=self.instance.pk)
+
         if user.exists():
-            raise forms.ValidationError("A user with that email already exists")
-        
+            raise forms.ValidationError(
+                "Користувач із такою електронною адресою вже існує")
+
         return self.cleaned_data.get('email')
 
     def change_password(self):
@@ -82,7 +83,7 @@ class UserProfileUpdateForm(forms.ModelForm):
             confirm_password = self.data['confirm_password']
             if new_password != '' and confirm_password != '':
                 if new_password != confirm_password:
-                    raise forms.ValidationError("Passwords do not match")
+                    raise forms.ValidationError("Паролі не збігаються")
                 else:
                     self.instance.set_password(new_password)
                     self.instance.save()
